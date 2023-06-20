2c84ffe1a606e09ec64adfccf8aa4e834fdfa20d
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-20 22:29:00 +0530

    added routes

--- basic.py
+++ basic.py
@@ -3,10 +3,18 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+#127.0.0.1:5000
 
 
 def index():
     return "<h1>Hello Puppy!</h1>"
+
+@app.route("/information")
+
+def info():
+    return "<h1> Puppies are cute!</h1>"
+
+
 
 
 
