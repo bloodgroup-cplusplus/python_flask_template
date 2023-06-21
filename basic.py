d8ce0296728a9160469f6f8cbaceb4ed32b4b375
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-21 06:00:49 +0530

    passing template variables

--- basic.py
+++ basic.py
@@ -39,7 +39,10 @@ app=Flask(__name__)
 @app.route("/")
 
 def index():
-    return render_template("basic.html")
+    name="Jose"
+    letters=list(name)
+    pup_dictionary={"pup_name":"Sammy"}
+    return render_template("basic.html",name=name,letters=letters,pup_dictionary=pup_dictionary)
 
 
 if __name__ =="__main__":
