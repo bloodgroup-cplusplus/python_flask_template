9efe5d23992940cfe2da732238ee3a836797e811
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-21 05:54:03 +0530

    added variables

--- basic.py
+++ basic.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+'''from flask import Flask 
 
 app = Flask(__name__)
 
@@ -29,4 +29,18 @@ def puppylatin(name):
 
 
 if __name__ == "__main__":
+    app.run(debug=True)'''
+
+
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+
+def index():
+    return render_template("basic.html")
+
+
+if __name__ =="__main__":
     app.run(debug=True)
